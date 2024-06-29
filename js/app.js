a7a6266b856f7f3aed1e5364ef3feb37f1fe88ec
lexandree7 <alexandrecna@gmail.com>
2024-06-29 15:56:10 -0300

    JavaScript ForEach example

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let name = "Ouroboros"
+console.log(`Hello ${name}, welcome!`);
+
+const numberA = 11
+const numberB = 27
+
+let total = numberA + numberB
+
+console.log(`The total is ${total}`);
+
+console.log(`The numberA variable is `, typeof numberA)
+
+//prettier-ignore
+const nameList = [
+    `Ouroboros`,
+    `Luffy`,
+    `Goku`,
+    `Saitama`
+]
+
+function print(text){
+    console.log(text);
+}
+
+function fancyPrint(text){
+    console.log(`--->💀 ${text} 💀 <----`);
+
+}
+
+nameList.forEach(fancyPrint);
+
